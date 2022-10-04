4b40a783c15677e3fdaac76b273ed2fdf36a5ec9
FimFi <55874743+FimFi@users.noreply.github.com>
2022-10-04 16:40:28 +0200

    meh

--- CustomerApi/Infrastructure/Listener.cs
+++ CustomerApi/Infrastructure/Listener.cs
@@ -21,8 +21,6 @@ namespace CustomerApi.Infrastructure
         {
             using (bus = RabbitHutch.CreateBus(_connectionString))
             {
-                bus.PubSub.Subscribe<CreateOrderMessage>("customerApiCreated", HandleOrderCreated);
-                bus.PubSub.Subscribe<PayOrderMessage>("customerApiPay", HandleOrderPaid);
 
                 lock (this)
                 {
@@ -31,81 +29,5 @@ namespace CustomerApi.Infrastructure
             }
         }
 
-        private void HandleOrderPaid(PayOrderMessage message)
-        {
-            using (var scope = _provider.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                var customerRepos = services.GetService<IRepository<Customer>>();
-
-                if (!CustomerHasGoodStanding(message.CustomerId, customerRepos))
-                {
-                    var customer = customerRepos.Get(message.CustomerId);
-                    customer.CreditStanding = true;
-                    customerRepos.Edit(customer);
-
-                    var replyMessage = new AcceptedPaidOrderMessage
-                    {
-                        OrderId = message.OrderId
-                    };
-
-                    bus.PubSub.Publish(replyMessage);
-                }
-                else
-                {
-                    var replyMessage = new RejectOrderPaidMessage
-                    {
-                        OrderId = message.OrderId
-                    };
-
-                    bus.PubSub.Publish(replyMessage);
-                }
-            }
-        }
-
-        private void HandleOrderCreated(CreateOrderMessage message)
-        {
-            using (var scope = _provider.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                var customerRepos = services.GetService<IRepository<Customer>>();
-
-                if (CustomerHasGoodStanding(message.CustomerId, customerRepos))
-                {
-                    var customer = customerRepos.Get(message.CustomerId);
-                    customer.CreditStanding = false;
-                    customerRepos.Edit(customer);
-
-                    var replyMessage = new AcceptedOrderMessage
-                    {
-                        OrderId = message.OrderId
-                    };
-
-                    bus.PubSub.Publish(replyMessage);
-                }
-                else
-                {
-                    var replyMessage = new RejectOrderMessage
-                    {
-                        OrderId = message.OrderId
-                    };
-
-                    bus.PubSub.Publish(replyMessage);
-                }
-            }
-        }
-
-        private bool CustomerHasGoodStanding(int customerId, IRepository<Customer> customerRepos)
-        {
-            var customer = customerRepos.Get(customerId);
-            if (customer != null && customer.CreditStanding)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
     }
 }
